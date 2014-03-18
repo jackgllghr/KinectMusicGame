abf97751a086f3b8dd15746702b7d3036ce29a0c
Jack Gallagher <jackgllghr@gmail.com>
2014-03-18 20:30:24 +0000

    Added a mute button for tracks

--- MusicGame/Track.cs
+++ MusicGame/Track.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Windows.Controls;
 using System.Windows.Shapes;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 
 namespace MusicGame
@@ -25,14 +26,24 @@ namespace MusicGame
       /// </summary>
       public String type { get; set; }
 
+      /// <summary>
+      /// The UI elements of the track
+      /// </summary>
       public Canvas trackUI { get; set; }
 
-      public Rectangle[] slots { get; set; }
       /// <summary>
-      /// The constructor takes the length the track needs to be and the 
+      /// The UI "slots" that the samples go into
+      /// </summary>
+      public Rectangle[] slots { get; set; }
+
+      Image muteIcon, volumeIcon;
+      public Microsoft.Kinect.Toolkit.Controls.KinectTileButton mute { get; set; }
+      /// <summary>
+      /// The constructor takes the length the track needs to be, the type of track, and which order number it is. This is used to set the UI  
       /// </summary>
       /// <param name="len"></param>
-      /// <param name="trackType"></param>
+      /// <param name="trackType">What type the track is(guitar/bass/drums)</param>
+      /// <param name="trackNo">The order number in the collection of tracks</param>
       public Track(int len, String trackType, int trackNo) {
         samples=new Sample[len];
 
@@ -60,8 +71,38 @@ namespace MusicGame
             };
             trackUI.Children.Add(slots[i]);
         }
+
+        muteIcon = new Image();
+        volumeIcon = new Image();
+        muteIcon.Source = new BitmapImage(new Uri("Assets/Icons/muteIconWhite.png", UriKind.Relative));
+        volumeIcon.Source = new BitmapImage(new Uri("Assets/Icons/volumeIconWhite.png", UriKind.Relative));
+        
+        mute = new Microsoft.Kinect.Toolkit.Controls.KinectTileButton
+        {
+            Margin = new System.Windows.Thickness(101 * trackLength, 0, 0, 0),
+            Width = 100,
+            Height = 100,
+            Foreground = Brushes.White,
+            Background = Brushes.RoyalBlue,
+
+        };
+        
+        mute.Content = volumeIcon;
+        mute.Click+=new System.Windows.RoutedEventHandler(MuteButtonClick);
+        trackUI.Children.Add(mute);
       }
-      //Add sample to track
+
+      private void MuteButtonClick(object sender, EventArgs e)
+      {
+          MuteTrack();
+          if (isPlaying) mute.Content = volumeIcon;
+          else mute.Content = muteIcon;
+      }
+      /// <summary>
+      /// Adds the given Sample to the given slot
+      /// </summary>
+      /// <param name="i">The slot to insert the sample</param>
+      /// <param name="s">The sample object that contains audio data</param>
       public void addSample(int i, Sample s) {
           if (s.type == type)
           {
@@ -69,7 +110,6 @@ namespace MusicGame
               s.isMoving = false;
           }
       }
- 
       /// <summary>
       /// Removes a sample from the track, where i is the slot with the sample to be removed
       /// </summary>
@@ -77,21 +117,34 @@ namespace MusicGame
       public void removeSample(int i) {
         samples[i]=null;
       }
+      /// <summary>
+      /// Swaps the samples in two slots, useful when a user drops a sample on top of another sample
+      /// </summary>
+      /// <param name="i">Number of one of the slots containing a sample</param>
+      /// <param name="j">Number of one of the slots containing a sample</param>
       public void SwapSamples(int i, int j)
       {
           Sample temp = samples[i];
           samples[i] = samples[j];
           samples[j] = temp;
       }
+      /// <summary>
+      /// Plays the audio data in the sample at the given slot(denoted by time)
+      /// </summary>
+      /// <param name="time">The slot number will be played</param>
       public void play(int time) {
         if (isPlaying && samples[time]!=null) {
           samples[time].play();
         }
       }
-      public void pause()
+      /// <summary>
+      /// Sets the current track to pause or muted so the user can isolate tracks
+      /// </summary>
+      public void MuteTrack()
       {
           isPlaying = !isPlaying;
       }
+
       public void AddSamplesRandomly(Sample[] samps)
       {
           Random r=new Random();
